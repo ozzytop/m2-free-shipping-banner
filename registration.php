4a2059823fb34f4ed2e3b6513e2e7157f6adc497
Matias Penas <matiaspenas@Matiass-MacBook-Pro-2.local>
2021-06-28 00:17:31 -0300

    Minimal code for creating a UI component

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Ozzytop_FreeShippingPromo',
+    __DIR__
+);
---
+++ view/frontend/web/js/free-shipping-banner.js
@@ -0,0 +1,12 @@
+define([
+    'uiComponent'
+], function(
+    Component
+) {
+    'use strict';
+
+    console.log('free shipping banner has been loaded');
+
+    return Component;
+
+})
