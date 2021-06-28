61fad07e0723a8329c21f6f12049001e52691d37
Matias Penas <matiaspenas@Matiass-MacBook-Pro-2.local>
2021-06-28 00:35:39 -0300

    Adding defaults property

--- view/frontend/web/js/free-shipping-banner.js
+++ view/frontend/web/js/free-shipping-banner.js
@@ -8,12 +8,19 @@ define([
 
     return Component.extend({
 
+        // Properties could be like variables of the component
+        defaults: {
+            message: 'Free Shipping Message'
+        },
+
         // This is called when the component is initialized
         initialize: function() {
 
-            // call the initialize function of the Component class. Imporntant to have the core functionality
+            // Call the initialize function of the Component class. Important to have the core functionality
             this._super();
             console.log('free shipping banner has been loaded');
+
+            console.log(this.message);
 
         }
     });
