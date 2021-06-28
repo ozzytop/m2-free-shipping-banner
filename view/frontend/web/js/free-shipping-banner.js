36c5c77386e11633eb38efdca3c39d62b3db633b
Matias Penas <matiaspenas@Matiass-MacBook-Pro-2.local>
2021-06-28 00:33:01 -0300

    Adding initialize to the component

--- view/frontend/web/js/free-shipping-banner.js
+++ view/frontend/web/js/free-shipping-banner.js
@@ -5,8 +5,17 @@ define([
 ) {
     'use strict';
 
-    console.log('free shipping banner has been loaded');
 
-    return Component;
+    return Component.extend({
+
+        // This is called when the component is initialized
+        initialize: function() {
+
+            // call the initialize function of the Component class. Imporntant to have the core functionality
+            this._super();
+            console.log('free shipping banner has been loaded');
+
+        }
+    });
 
 })
